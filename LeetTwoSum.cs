c78e9e98c590ed979de05ba9a88c75feb805f8d1
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-12-13 07:28:11 -0500

    Examples 1,2,3

--- LeetTwoSum.cs
+++ LeetTwoSum.cs
@@ -1,40 +1,6 @@
-﻿using System;
-class LeetTwoSum 
-{
-    public class Solution {
-        public int[]? TwoSum(int[] nums, int target) {
-            int[] arrayWithResults = new int[2];
-            for (int i = 0; i < nums.Length; i++)
-            {
-                for (int j = 0; j < nums.Length; j++)
-                {
-       
-                    if (nums[i] + nums[j] == target)
-                    {
-                        arrayWithResults[0] = i;
-                        arrayWithResults[1] = j;
-                        return arrayWithResults;                        
-                    }  
-                }
-            }
-            return null;
-        }
-    }
-    
-
-    static void Main(string[] args)
-    {
-        Solution s = new Solution();
-        int[] nums1 = [1,3,4,9];
-        int target1 = 4;
-        int[] result = s.TwoSum(nums1, target1);
+﻿public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
         
-        Console.WriteLine(string.Join(", ", result));
-  
-
-
-
-
+            return 0;
     }
 }
-
